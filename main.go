d3acb68285f29c2628bc392bb8b73743e2cc2880
guaiu <guaiu@global.com>
2022-06-30 20:05:31 +0800

    1. 实现http.Handler接口

--- main.go
+++ main.go
@@ -1,25 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/guaiu/myGee/web"
 )
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/hello", helloHandler)
-	log.Fatal(http.ListenAndServe(":9999", nil))
-}
-
-// handler echoes r.URL.Path
-func indexHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
-}
-
-//handler echoes r.URL.Path
-func helloHandler(w http.ResponseWriter, req *http.Request) {
-	for k, v := range req.Header {
-		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-	}
+	engine := new(web.Engine)
+	log.Fatal(http.ListenAndServe(":9999", engine))
 }
