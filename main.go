492b867f3cb834c05a07adef85f45b5910b7ec8d
guaiu <guaiu@global.com>
2022-07-11 19:41:55 +0800

    4.使用前缀树实现动态路由

--- main.go
+++ main.go
@@ -9,19 +9,21 @@ import (
 func main() {
 	r := web.New()
 	r.GET("/", func(c *web.Context) {
-		c.HTML(http.StatusOK, "<h1>hello, myGee</h1>")
+		c.HTML(http.StatusOK, "<h1>Hello myGee</h1>")
 	})
 
 	r.GET("/hello", func(c *web.Context) {
 		// expect /hello?name=guaiu
-		c.String(http.StatusOK, "hello, %s, you're at %s\n", c.Query("name"), c.Path)
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
 
-	r.POST("/login", func(c *web.Context) {
-		c.JSON(http.StatusOK, web.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
-		})
+	r.GET("/hello/:name", func(c *web.Context) {
+		// expect /hello/guaiu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *web.Context) {
+		c.JSON(http.StatusOK, web.H{"filepath": c.Param("filepath")})
 	})
 
 	r.Run(":9999")
